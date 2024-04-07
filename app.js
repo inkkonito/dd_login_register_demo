e1114340b4c53077cadc7ea16aa97d793655e1a1
inkkonito <pro.julienp@gmail.com>
2024-04-07 21:28:25 +0200

    added DataDome design and colors to the form

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/register.html");
 });
 
-app.use(express.static(__dirname + "."));
+app.use(express.static(__dirname + "/public"));
 
 app.use(
   sessions({
